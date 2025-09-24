9809e8376b008fd44f0b97ea3c845e9571a03437
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-09-24 20:06:36 +0200

    feat: Adding data to the home page
    Linking the data to the home page.
    Setting up a FlatList and styling.
    Correcting the links to the images in the product data file.

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,13 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { PRODUCTS } from "../../data/products";
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContainer}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContainer: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: "space-between",
+  },
+});
