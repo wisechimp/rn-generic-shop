3c8891f1bb636308ffacfcbce0e6549b3e1cbfb4
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2025-09-26 13:42:48 +0200

    Squashed commit of the following:
    commit cc19881d6af5f7615804e981aac63c21e877fe9c
    Merge: f133be5 b36be72
    Author: Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
    Date:   Fri Sep 26 13:39:26 2025 +0200
    
        Merge pull request #6 from wisechimp/4-populate-the-home-page
    
        4 populate the home page
    
    commit b36be7299e8c6bdcefac421d470d4f4dcf7ab67c
    Author: Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
    Date:   Fri Sep 26 13:34:47 2025 +0200
    
        feat: Adding the home page information
    
        There's a header component with a little user info and functionality, hero image and list of categories.
        There's a list component with all the products available.
    
    commit 9809e8376b008fd44f0b97ea3c845e9571a03437
    Author: Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
    Date:   Wed Sep 24 20:06:36 2025 +0200
    
        feat: Adding data to the home page
    
        Linking the data to the home page.
        Setting up a FlatList and styling.
        Correcting the links to the images in the product data file.
    
    commit 73243cfa75a40e54f6d57419bbd3ab943a41f034
    Author: Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
    Date:   Wed Sep 24 18:13:38 2025 +0200
    
        feat: Adding project's assets

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,13 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { PRODUCTS } from "../../data/products";
+import ProductListItem from "../../components/products-list/products-list-item";
+import ListHeader from "../../components/products-list/list-header";
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => {
+          return <ProductListItem product={item} />;
+        }}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<ListHeader />}
+        contentContainerStyle={styles.flatListContainer}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContainer: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: "space-between",
+  },
+});
---
+++ src/components/products-list/products-list-item/index.tsx
@@ -0,0 +1,58 @@
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { Product } from "../../../types/product";
+import { Link } from "expo-router";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <Link asChild href={`/product/${product.slug}`}>
+      <Pressable style={styles.product}>
+        <View style={styles.productImageContainer}>
+          <Image source={product.heroImage} style={styles.productImage} />
+        </View>
+        <View style={styles.productTextContainer}>
+          <Text style={styles.productTitle}>{product.title}</Text>
+          <Text style={styles.productPrice}>${product.price.toFixed(2)}</Text>
+        </View>
+      </Pressable>
+    </Link>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  product: {
+    width: "48%",
+    backgroundColor: "white",
+    marginVertical: 8,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  productImageContainer: {
+    borderRadius: 10,
+    width: "100%",
+    height: 150,
+  },
+  productImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+  productTextContainer: {
+    padding: 8,
+    alignItems: "flex-start",
+    gap: 4,
+  },
+  productTitle: {
+    fontSize: 16,
+    color: "#888",
+  },
+  productPrice: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+});
